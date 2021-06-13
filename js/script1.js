288061608bd65df1a1e73f6bdb9e876352908a9e
Danil <domnyshev.danil@mail.ru>
2021-06-14 00:29:09 +0500

    First commit

---
+++ js/script1.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let number = 4;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+console.log(persone);
+
+const persone = '5';
+
+const bool = true;
+
+console.lor(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    iaMaried: false
+};
+
+console.log(obj["name"]);
+
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[0]);
---
+++ js/script2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/* alert('Hello!'); */
+
+/* const result = confirm('Are you hear?');
+ */
+/* console.log(result);
+ */
+
+/* const answer = prompt('Вам есть 18?', "18");
+console.log(typeof(answer)); */
+
+const answers = [];
+
+answers[0] = prompt('W y n ?', '');
+answers[1] = prompt('Family ?', '');
+answers[2] = prompt('H o a y ?', '');
+
+console.log(typeof(answers));
+
