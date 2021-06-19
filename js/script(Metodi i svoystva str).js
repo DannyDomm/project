c8a2aa65d55e56a84aca1241e5f19358f043f28b
Danil <domnyshev.danil@mail.ru>
2021-06-20 00:01:41 +0500

    уроки

---
+++ js/script(Metodi i svoystva str).js
@@ -0,0 +1,26 @@
+"use strict"
+
+const str = "test";
+const arr = [1, 2, 4];
+
+console.log(str.toUpperCase());
+
+
+let fruit = "some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt());
+console.log(parseFloat());
--- js/script.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-console.log("arr " + "- object");
-console.log(4 + "- object")
